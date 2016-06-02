e6b8de3130f1412dcdbc3408492c18065291b48c
sergeyradkov <reriic@ya.ru>
2016-06-01 23:58:08 -0600

    123

--- game.js
+++ game.js
@@ -23,9 +23,9 @@ var item = function (name, modifier, description){
 }
 
 var items = {
-    helmet: new item("Helmet",  1,"an awesome helmet!"),
-    shield: new item("Shield",  3,"an awesome shield!"),    
-      vest: new item("Vest",    5,"an awesome vest!"),
+    cap: new item("Cap", 1,"an awesome cap!"),
+    glass: new item("Sunglasses", 3,"an awesome sunglasses!"),    
+    chain: new item("Chain", 5, "an awesome golden chain!"),
 }
 
 var player = { items:[] }
@@ -34,26 +34,20 @@ var player = { items:[] }
 function addMods(x) {
       
     var item = items[x];
-        
-    if(player.items.indexOf(item) < 0) {
-        player.items.push(item);
-        } else { alert ("He has " + item.description);
-    } 
+    $("#btn-"+x).attr('disabled',true);
     
     safe = 0;
     for (var i=0; i < player.items.length; i++ ){
         safe = safe + player.items[i].modifier;
     }
   
-    document.getElementById('safe').innerText = safe;
+    $("#safe").text = safe;
 }
 
 function hit(x){
     
     if(health <= 0) return
-    
     var damage = hitDamages[x].damage - safe;
- 
     health = health - damage;
     hits++;
     update();  
@@ -66,8 +60,8 @@ function update() {
     
     
     if(health <= 0){
-         document.getElementById("player-panel").classList.add("panel-danger")
+         document.getElementById("player-box").classList.add("game-over")
     }else{
-         document.getElementById("player-panel").classList.remove("panel-danger")
+         document.getElementById("player-box").classList.remove("game-over")
     }
 }
